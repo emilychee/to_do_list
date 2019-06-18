40bbbc21d964518f9ee73d63bff5c127fa9bdca6
Emily.Chatterjee <z0041gf@34363bd00e70.hq.target.com>
2019-06-18 16:26:24 -0500

    working on styling the list items

--- static/js/script.js
+++ static/js/script.js
@@ -6,9 +6,12 @@
 
 			$scope.addItem = function(taskname) {
 				$scope.li = angular.element('<li/>');
+				$scope.li.attr("class", "list-group-item list-group-item-action");
 				$scope.li.append(document.createTextNode(taskname));
 				var list = document.getElementById("list");
 				angular.element(list).append($scope.li);
+				// reset input field
+				document.getElementById("input").value = '';
 			};
 		}
 	]);
