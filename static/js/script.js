b7c46706bdcef81dd29c95940f009cd3dec46d0f
Emily.Chatterjee <z0041gf@34363bd00e70.hq.target.com>
2019-06-19 16:22:51 -0500

    working on crossing out items

--- static/js/script.js
+++ static/js/script.js
@@ -3,12 +3,13 @@
 
 	angular.module('toDo', []).controller('MainCtrl', ['$scope',
 		function($scope) {
+			$scope.dict = {};
 
 			$scope.addItem = function(taskname) {
 				// make list element
 				$scope.li = angular.element('<li/>');
 				$scope.li.attr("class", "list-group-item list-group-item-action");
-				$scope.li.attr("id", "")
+				$scope.li.attr("ng-click", "crossOut()")
 				// append to DOM
 				$scope.li.append(document.createTextNode(taskname));
 				var list = document.getElementById("list");
@@ -16,6 +17,15 @@
 				// reset input field
 				document.getElementById("input").value = '';
 			};
+
+			$scope.crossOut = function() {
+				var text = document.querySelector("li").value;
+				document.querySelector("li").value = text.strike();
+			};
+			/*@HostListener('click', ['$event.target'])
+			onClick(target) {
+				console.log("hello");
+			}*/
 		}
 	]);
 }());
